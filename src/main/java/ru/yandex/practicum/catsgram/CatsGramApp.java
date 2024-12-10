e9bc0c3ceb86c6cf1a55097803b94705fbaa0732
Just Roma <romke411@gmail.com>
2024-12-10 13:23:09 +0500

    Start project

---
+++ src/main/java/ru/yandex/practicum/catsgram/CatsGramApp.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.catsgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsGramApp {
+    public static void main(String[] args) {
+
+        SpringApplication.run(CatsGramApp.class, args);
+
+    }
+}
